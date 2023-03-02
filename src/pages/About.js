cd22bcb070a2e29981f00045644ef4ce3261a0d6
Debajit Mallick <42708550+debajit13@users.noreply.github.com>
2023-03-02 20:55:55 +0530

    integrate api and about page ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,28 @@
+import { Card, CardBody, CardHeader, CardText, Container } from 'reactstrap';
+
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <Container className='text-center mt-3'>
+      <Card color='dark' outline>
+        <CardHeader>
+          <h1>About</h1>
+        </CardHeader>
+        <CardBody>
+          <CardText>
+            Short Link is a one stop solution for your link shortening task. In
+            this digital era, links are our daily friends. But most of the time
+            the lenght of the link is what makes it our reason for frustration.
+            Maybe the message app that you use have certain length limit, or the
+            tweet you want to do not support that much long link. In situations
+            like this Short Link comes into picture to make your life easy.
+          </CardText>
+          <CardText>
+            Short Link under the hood uses SHRTCODE API, ReactJS, ReactStrap,
+            Bootstrap, React Toastify, React Router and Axios.
+          </CardText>
+        </CardBody>
+      </Card>
+    </Container>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,10 +10,41 @@ import {
   InputGroup,
 } from 'reactstrap';
 import { BsClipboardCheck } from 'react-icons/bs';
+import { ToastContainer, toast } from 'react-toastify';
+import { getShortURL } from '../api';
 
 const Home = () => {
   const [longURL, setLongURL] = useState('');
   const [shortURL, setShortURL] = useState('');
+
+  async function shortURLCopyHandler(url) {
+    try {
+      await navigator.clipboard.writeText(url);
+      toast.success('short URL copied successfully!', {
+        position: 'top-right',
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+    } catch (err) {
+      if (err) {
+        toast.error('Failed to copy short URL!', {
+          position: 'top-right',
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'dark',
+        });
+      }
+    }
+  }
 
   const longURLInputHandler = (e) => {
     setLongURL(e.target.value);
@@ -22,18 +53,26 @@ const Home = () => {
 
   const longURLSubmitHandler = (e) => {
     e.preventDefault();
-    setShortURL(longURL);
+    getShortURL(longURL).then((res) => {
+      if (res?.data.ok) {
+        setShortURL(res?.data.result.short_link);
+      }
+    });
+
     setLongURL('');
   };
 
   return (
-    <Container className='d-flex flex-column align-items-center mt-3 justify-content-center'>
+    <Container className='d-flex h-75 flex-column align-items-center mt-3 justify-content-center'>
+      <ToastContainer />
+
       <Card style={{ maxWidth: '500px', width: '100%' }}>
         <CardBody>
           <Form
             onSubmit={(e) => {
               longURLSubmitHandler(e);
             }}
+            className='mt-3'
           >
             <FormGroup>
               <InputGroup>
@@ -59,7 +98,12 @@ const Home = () => {
             <Card color='dark' outline>
               <CardBody className='d-flex justify-content-between align-items-center'>
                 <span>{shortURL}</span>
-                <span>
+                <span
+                  role='button'
+                  onClick={() => {
+                    shortURLCopyHandler(shortURL);
+                  }}
+                >
                   <BsClipboardCheck size={28} color='#000' />
                 </span>
               </CardBody>
